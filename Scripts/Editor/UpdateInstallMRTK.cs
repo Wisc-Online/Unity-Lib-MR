2536e7a33ae45d42461bfd9708c1a453f4925be6
Brett Sheleski <brett@sheleski.com>
2018-10-19 12:06:34 -0500

    updated menu

--- Scripts/Editor/UpdateInstallMRTK.cs
+++ Scripts/Editor/UpdateInstallMRTK.cs
@@ -15,6 +15,28 @@ namespace FVTC.LearningInnovations.Unity.MixedReality.Editor
     {
 
         const string MRTK_GITHUB_URL = "https://github.com/Microsoft/MixedRealityToolkit-Unity.git";
+        const string UNITY_LIB_MR_GITHUB_URL = "https://github.com/Wisc-Online/Unity-Lib-MR.git";
+
+
+        [MenuItem("Learning Innovations/Mixed Reality/Update from GitHub")]
+        private static void Update()
+        {
+            if (GitHelper.UpdateSubmodule(UNITY_LIB_MR_GITHUB_URL))
+            {
+                AssetDatabase.Refresh();
+            }
+            else
+            {
+                Dialog.Close("Update Failed", "Updating from GitHub failed.  See console for details.");
+            }
+        }
+
+        [MenuItem("Learning Innovations/Mixed Reality/Update from GitHub", true)]
+        private static bool ValidateUpdate()
+        {
+            return GitHelper.GetModules().Where(x => UNITY_LIB_MR_GITHUB_URL.Equals(x.Url.AbsoluteUri, StringComparison.OrdinalIgnoreCase)).Any();
+        }
+
 
         [MenuItem("Learning Innovations/Mixed Reality/Install MRTK from GitHub")]
         private static void UpdateInstallMRTK()
